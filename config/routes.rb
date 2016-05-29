7d301a89539b5598de353b818372a01c98f89417
Jordan Deutsch <jordanbdeutsch@gmail.com>
2016-05-29 15:32:44 -0400

    admin for test

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Rails.application.routes.draw do
     resources :learning_modules
     resources :videos
     resources :skin_diets
+    resources :tests
    end 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
