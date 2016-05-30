a797776d5a500aef60e16b030f420c12191928a1
deartrudence <trudy.macnabb@gmail.com>
2016-05-30 15:16:30 -0400

    forms again

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,5 @@
 class Profile < ActiveRecord::Base
+	PROVINCES = ['Ontario', 'Quebec', 'British Columbia', 'Alberta', 'Manitoba', 'Saskatchewan', 'Nova Scotia', 'New Brunswick', 'NewfoundLand & Labrador', 'Prince Edward Island', 'Northwest Territories', 'Yukon', 'Nunavut']
+	RETAILERS = ['Sephora', 'Shoppers Drug Mart']
 	belongs_to :user
 end
