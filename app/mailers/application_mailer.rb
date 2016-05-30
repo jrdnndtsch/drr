2e0ca2b25487f6555303613935091bde0f7bc366
Jordan Deutsch <jordanbdeutsch@gmail.com>
2016-05-30 15:49:21 -0400

    mailer

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "hello@upatfive.ca"
+  layout 'mailer'
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,6 +54,16 @@ Rails.application.configure do
     :bucket => 'drroebuck'
   }
 
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for mailgun
+  ActionMailer::Base.smtp_settings = {
+    :port           => 587,
+    :address        => "smtp.mailgun.org",
+    :domain         => ENV['domain'],
+    :user_name      => ENV['username'],
+    :password       => ENV['password'],
+    :authentication => :plain,
+  }
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
