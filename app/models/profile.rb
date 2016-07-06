b395606cbb32e8bc4459655f53831c5b5bdd797c
deartrudence <trudy.macnabb@gmail.com>
2016-07-06 16:53:39 -0400

    typo

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,7 @@
 class Profile < ActiveRecord::Base
 	PROVINCES = ['Ontario', 'Quebec', 'British Columbia', 'Alberta', 'Manitoba', 'Saskatchewan', 'Nova Scotia', 'New Brunswick', 'NewfoundLand & Labrador', 'Prince Edward Island', 'Northwest Territories', 'Yukon', 'Nunavut']
 	RETAILERS = ['Sephora', 'Shoppers Drug Mart']
-	SKIN_CONCERNS = ['No Concern', 'Acne', 'Dehydration', 'Preven Aging', 'Dark Spots', 'Dry Skin', 'Wrinkles', 'Oily']
+	SKIN_CONCERNS = ['No Concern', 'Acne', 'Dehydration', 'Prevent Aging', 'Dark Spots', 'Dry Skin', 'Wrinkles', 'Oily']
 	belongs_to :user
 
    def self.export_csv
